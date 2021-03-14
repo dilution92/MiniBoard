f388c3bc4b702f3c59685cc3a467fe11d826d129
dilution92 <qkswkd99@naver.com>
2021-03-14 23:27:04 +0900

    no message

---
+++ MiniBoard/src/main/java/com/board/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	
+	@RequestMapping(value = "/", method = {RequestMethod.GET,RequestMethod.POST} )
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("mainPage");
+		return mv;
+	}
+
+}
--- MiniBoard/src/main/resources/templates/index.jsp
+++ MiniBoard/src/main/resources/templates/index.jsp
@@ -18,5 +18,8 @@
 </head>
 <body>
 
+<h1>메인페이지</h1>
+
+
 </body>
 </html>
